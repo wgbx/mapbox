88b6881854bb941f2cff08962eaefaf62efa3657
微光北下 <wgbx@foxmail.com>
2019-08-01 17:11:00 +0800

    add 地图展示

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,34 +1,24 @@
 <template>
-  <div class="warp">
-    {{$store.state.home.show}}
-    <div class="item">123</div>
+  <div class="home">
+    <home-map></home-map>
   </div>
 </template>
 
 <script>
+  import HomeMap from "./components/map";
+
   export default {
     name: "Home",
+    components: {HomeMap},
     created() {
-      this.updata();
+
     },
-    methods: {
-      updata() {
-        let self = this;
-        setTimeout(function () {
-          self.$store.commit('switch');
-          self.$store.getters.reverse;
-        }, 2000)
-      }
-    }
+    methods: {}
   }
 </script>
 
 <style lang="scss" scoped>
-  .warp {
-    width: 1200px;
+  .home {
 
-    .item {
-
-    }
   }
 </style>
---
+++ src/pages/home/components/map.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="map"
+       ref="map">
+
+  </div>
+</template>
+
+<script>
+  import mapboxgl from 'mapbox-gl/dist/mapbox-gl'
+  import MapboxLanguage from '@mapbox/mapbox-gl-language'
+
+  export default {
+    name: 'homeMap',
+    data() {
+      return {
+        map: {},
+      }
+    },
+    mounted() {
+      this.initMap();
+    },
+    methods: {
+      initMap() {
+        let language = new MapboxLanguage();
+        mapboxgl.accessToken = 'pk.eyJ1Ijoid2dieCIsImEiOiJjanlzYmZ0cmcwYXI0M3BzNXNtZHJncGF2In0.J4xGHXLaGljxa6DwR2BctQ';
+        this.map = new mapboxgl.Map({
+          container: this.$refs.map,
+          style: 'mapbox://styles/mapbox/streets-v10',
+          center: [116.59108409724627, 40.08441341752007],
+          zoom: 11.5,
+          trackResize: true,
+        });
+        this.map.addControl(language);
+      },
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+  .map {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+  }
+</style>
