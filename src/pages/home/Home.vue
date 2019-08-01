56f7e6b59aeac2675effdbbd2c1dd3ca32fb38ed
微光北下 <wgbx@foxmail.com>
2019-08-01 16:07:25 +0800

    del home-vuex

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,34 +1,17 @@
 <template>
-  <div class="warp">
-    {{$store.state.home.show}}
-    <div class="item">123</div>
+  <div class="home">
+
   </div>
 </template>
 
 <script>
   export default {
     name: "Home",
-    created() {
-      this.updata();
-    },
-    methods: {
-      updata() {
-        let self = this;
-        setTimeout(function () {
-          self.$store.commit('switch');
-          self.$store.getters.reverse;
-        }, 2000)
-      }
-    }
   }
 </script>
 
 <style lang="scss" scoped>
-  .warp {
-    width: 1200px;
-
-    .item {
-
-    }
+  .home {
+    
   }
 </style>
